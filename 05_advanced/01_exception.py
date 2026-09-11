3e753419612a9474e232103f0589478ef6977ee2
sjlimiei26 <sjlim.iei.26@gmail.com>
2026-09-11 11:54:15 +0900

    260911 파이썬 실습6

--- 05_advanced/01_exception.py
+++ 05_advanced/01_exception.py
@@ -67,6 +67,11 @@ acc = Account("이우진", 30000)
 print(acc)
 
 for amount in [5000, 50000, -1000]:
-    acc.withdraw(amount)
-
-# TODO: 마지막 반복문의(:69) 발생되는 예외를 처리 ( ~ 18:00 이메일 제출)
+    try:
+        acc.withdraw(amount)
+    except NoBalanceError as e:
+        print(f"출금 실패 : {e}")
+    except InvalidAmountError as e:
+        print(f"출금 실패 : {e}")
+    else:
+        print(f"출금 성공 : {amount}원")
